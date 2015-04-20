b60f0a7e7a1c520bb3f5295207aded703333de47
Jonah Karpinski <Jkarpinski14@yahoo.com>
2015-04-20 10:22:54 -0700

    Assessment completed

---
+++ jquery.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+ 
+	$( ".dan" ).click(function() {
+    	alert( "Hey you clicked on me" );
+	});
+
+    $( "#iwao" ).click(function() {
+    	alert( "Hey you clicked on me again" );
+	});
+
+    //alert( "welcome" );
+};
